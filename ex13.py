827a151e0d3bfc1f2ff613cec77a62fdf009a477
ainaosk <39031705+ainaosk@users.noreply.github.com>
2018-08-30 19:08:52 +0200

    ex13.py

---
+++ ex13.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Learn Python the Hard Way: Exercise 13
+"""
+from sys import argv
+
+script, first, second, third = argv
+
+print("The script is called: ", script)
+print("The first variable is: ", first)
+print("The second variable is: ", second)
+print("The third variable is: ", third)
+
+reply = input("Hey, how are you?")
